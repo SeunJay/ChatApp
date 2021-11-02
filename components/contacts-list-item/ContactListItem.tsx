d67bdf32a27e389afdc616e30789f8a614b3bb23
Oluwaseun Jayeoba <seunjay92@gmail.com>
2021-11-02 12:39:48 +0100

    create chat room

--- components/contacts-list-item/ContactListItem.tsx
+++ components/contacts-list-item/ContactListItem.tsx
@@ -5,11 +5,17 @@ import {
   View,
   Image,
   TouchableWithoutFeedback,
+  TouchableOpacity,
 } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
+import { API, graphqlOperation, Auth } from 'aws-amplify';
+import moment from 'moment';
 
 import { User } from '../../types';
-import moment from 'moment';
+import {
+  createChatRoom,
+  createChatRoomUser,
+} from '../../src/graphql/mutations';
 
 export type ContactListItemProps = {
   user: User;
@@ -20,9 +26,48 @@ const ContactListItem = (props: ContactListItemProps) => {
 
   const navigation = useNavigation();
 
-  const handleClick = () => {};
+  const handleClick = async () => {
+    try {
+      // 1. create chat room
+      const chatRoomData = await API.graphql(
+        graphqlOperation(createChatRoom, { input: {} })
+      );
+      if (!chatRoomData.data) return console.log('failed to create chat room');
+
+      const newChatRoom = chatRoomData.data.createChatRoom;
+      // console.log(newChatRoom);
+
+      // 2. add user to chat room
+      await API.graphql(
+        graphqlOperation(createChatRoomUser, {
+          input: {
+            userID: user.id,
+            chatRoomID: newChatRoom.id,
+          },
+        })
+      );
+
+      // 3. add authenticated user to chat room
+      const userInfo = await Auth.currentAuthenticatedUser();
+      await API.graphql(
+        graphqlOperation(createChatRoomUser, {
+          input: {
+            userID: userInfo.attributes.sub,
+            chatRoomID: newChatRoom.id,
+          },
+        })
+      );
+
+      navigation.navigate('ChatRoom', {
+        id: newChatRoom.id,
+        name: user.name,
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  };
   return (
-    <TouchableWithoutFeedback onPress={handleClick}>
+    <TouchableOpacity onPress={handleClick}>
       <View style={styles.container}>
         <View style={styles.leftContainer}>
           <Image
@@ -37,7 +82,7 @@ const ContactListItem = (props: ContactListItemProps) => {
           </View>
         </View>
       </View>
-    </TouchableWithoutFeedback>
+    </TouchableOpacity>
   );
 };
 
